0d0edfe908d2e1f214eb2f4e318a3b9020c97df8
haybek <haybekbatuhan@gmail.com>
2023-07-07 11:41:37 +0300

    son

--- 7TemmuzTeori/Form1.Designer.cs
+++ 7TemmuzTeori/Form1.Designer.cs
@@ -28,12 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(600, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 72;
+            dataGridView1.RowTemplate.Height = 37;
+            dataGridView1.Size = new Size(723, 739);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(562, 40);
+            button1.TabIndex = 1;
+            button1.Text = "Ogrenciler Id Ad Soyad";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(3, 67);
+            button2.Name = "button2";
+            button2.Size = new Size(562, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Ogrencinin tam adi takim liderinin tam adi ve takim liderlerinin ismi";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(3, 122);
+            button3.Name = "button3";
+            button3.Size = new Size(562, 40);
+            button3.TabIndex = 1;
+            button3.Text = "Ogrencinin adi soyadi ve sevdigi sehirler";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(3, 178);
+            button4.Name = "button4";
+            button4.Size = new Size(562, 40);
+            button4.TabIndex = 1;
+            button4.Text = "Adinda veya soyadinda e harfi gecenler";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(3, 224);
+            button5.Name = "button5";
+            button5.Size = new Size(562, 40);
+            button5.TabIndex = 1;
+            button5.Text = "Ogrencinin adi dogdugu yer ve nufusu";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(3, 278);
+            button6.Name = "button6";
+            button6.Size = new Size(562, 40);
+            button6.TabIndex = 1;
+            button6.Text = "Erkeklerin sevdigi sehilerin plaka numarasina gore sirali getir";
+            button6.TextAlign = ContentAlignment.MiddleLeft;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(3, 334);
+            button7.Name = "button7";
+            button7.Size = new Size(562, 40);
+            button7.TabIndex = 1;
+            button7.Text = "Ogrencinin tam adi email adres ve telefonu";
+            button7.TextAlign = ContentAlignment.MiddleLeft;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(3, 389);
+            button8.Name = "button8";
+            button8.Size = new Size(562, 40);
+            button8.TabIndex = 1;
+            button8.Text = "Soyadi dogdugu sehrin isminden uzun olanlar ";
+            button8.TextAlign = ContentAlignment.MiddleLeft;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(3, 451);
+            button9.Name = "button9";
+            button9.Size = new Size(562, 40);
+            button9.TabIndex = 1;
+            button9.Text = "Ogrenci id si adi ve idsinin bir fazlasina sahip ogrencinin adi";
+            button9.TextAlign = ContentAlignment.MiddleLeft;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(3, 497);
+            button10.Name = "button10";
+            button10.Size = new Size(562, 40);
+            button10.TabIndex = 1;
+            button10.Text = "Hangi sehir kac kisi tarafindan seviliyor plaka sehir ogrenci";
+            button10.TextAlign = ContentAlignment.MiddleLeft;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(3, 554);
+            button11.Name = "button11";
+            button11.Size = new Size(562, 40);
+            button11.TabIndex = 1;
+            button11.Text = "ogrencilerin adi soyadi ve covid gecirme dutumu ancak gecirdi gecirmedi";
+            button11.TextAlign = ContentAlignment.MiddleLeft;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(3, 600);
+            button12.Name = "button12";
+            button12.Size = new Size(562, 40);
+            button12.TabIndex = 1;
+            button12.Text = "Ogrenciler Id Ad Soyad";
+            button12.TextAlign = ContentAlignment.MiddleLeft;
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1335, 763);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button8);
+            Controls.Add(button10);
+            Controls.Add(button7);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button5);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
     }
 }
--- 7TemmuzTeori/Form1.cs
+++ 7TemmuzTeori/Form1.cs
@@ -1,10 +1,111 @@
+using _7TemmuzTeori.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
 namespace _7TemmuzTeori
 {
     public partial class Form1 : Form
     {
+
+
+        Boost13DbContext db = new Boost13DbContext();
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Boost13DbContext context = new Boost13DbContext())
+            {
+
+                //var result = context.Ogrenciler.SqlQuery("SELECT * FROM Ogrenciler")
+                //.ToList();
+                var result2 = context.Ogrenciler.Select(x => new { x.Id, x.Ad, x.Soyad }).ToList();
+
+                dataGridView1.DataSource = result2;
+
+                //var result3 = context;
+
+                //dataGridView1.DataSource = result3;
+
+                //var result = context.Ogrenciler.Select(x =>
+                //new { x.Ad, x.Soyad }).ToList();
+                //dataGridView1.DataSource = result;
+
+
+                //dataGridView1.AutoGenerateColumns = false; // Otomatik sütun üretimini devre dýþý býrakýn
+
+                //dataGridView1.Columns.Add("Ad", "Ad"); // "Ad" sütunu ekle
+                //dataGridView1.Columns.Add("Soyad", "Soyad"); // "Soyad" sütunu ekle
+
+                //var result = context.Ogrenciler.ToList();
+                //dataGridView1.DataSource = result;
+
+
+
+                // SQL sorgusunu tetikleme kodunu buraya yazabilirsiniz
+                // Örneðin: var result = context.TabloAdi.SqlQuery("SELECT * FROM TabloAdi").ToList();
+                //         foreach (var item in result)
+                //         {
+                //             // Verileri iþleme kodunu buraya yazabilirsiniz
+                //         }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+
+            using (Boost13DbContext context = new Boost13DbContext())
+            {
+
+
+                //var result2 = context.Ogrenciler.Include(x => x.TakimLideri).Select(x => new { x.Ad, x.Soyad, takimlideriad = x.TakimLideri.Ad, takimlideriyas = SqlFunctions.DateDiff("year", x.TakimLideri.DogumTarihi, DateTime.Now) }).ToList();
+
+                //dataGridView1.DataSource = result2;
+
+                dataGridView1.DataSource = context.Ogrenciler
+                 .Include(e => e.TakimLideri)
+                 .Select(e => new
+                 {
+                     e.Ad,
+                     e.Soyad,
+                     takimLider = e.TakimLideri.Ad,
+                     takimLiderYas = ((DateTime.Now - e.TakimLideri.DogumTarihi) / 365)
+                 })
+                 .ToList();
+
+                //var result2 = context.Ogrenciler.Include(x => x.Sehirler).Select(x => new { x.Id, x.Ad, x.Soyad }).ToList();
+
+                //dataGridView1.DataSource = result2;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            using (Boost13DbContext context = new Boost13DbContext())
+            {
+                dataGridView1.DataSource = context.Sehirler
+                .Include(e => e.Sevenler)
+                .Select(e => new
+                {
+                    e.SehirAd,
+                    SevenlerAd = string.Join(", ", e.Sevenler.Select(x => x.Ad))
+                }).ToList()
+        .Where(x => !string.IsNullOrEmpty(x.SevenlerAd));
+
+            }
         }
     }
 }
